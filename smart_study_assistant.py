42d49fefcaf03602ec7344b881d80c83d891f9ab
Abdul Wasi Bhat <bhatabdul75@gmail.com>
2025-07-16 14:10:50 +0530

    first commit

---
+++ smart_study_assistant.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Main application window
+root = tk.Tk()
+root.title("Smart Study Assistant")
+root.geometry("800x600")
+
+# Frames
+# Create frames for Layout
+
+# Start the main loop
+root.mainloop()
+
